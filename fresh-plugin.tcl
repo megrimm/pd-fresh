02eea66c01fc23608c96dc85d53dcd07d551f326
megrimm <megrimm@gmail.com>
2015-11-08 21:39:44 -0500

    various changes and updates

--- fresh-plugin.tcl
+++ fresh-plugin.tcl
@@ -3,17 +3,26 @@
 # META DESCRIPTION Lets you switch to and back from fullscreen with F11
 # META AUTHOR <András Murányi> muranyia@gmail.com
 
-::pdwindow::post "-"
-::pdwindow::post "Shift + Command + F = Fullscreen Patch"
-::pdwindow::post "-"
+::pdwindow::post "\n-\n"
+::pdwindow::post "Shift + Command + F = Fullscreen Patch\n"
+::pdwindow::post "-\n\n"
 
 package require Tcl 8.4
 package require Tk
 package require pdwindow 0.1
 package require pd_menus 0.1
 
+#
+# Set fresh/_extra/ user directory (NOT WORKING)
+#
+set custom_search_pathset {
+	{fresh}{_extra}
+	}
+set enabled_libraries {fresh}
 
+#
 # Fullscreen Patch Window
+#
 variable fullscreen_windows {}
 
 proc go_fullscreen {window} {
@@ -53,26 +62,127 @@ proc toggle_fullscreen {window} {
 	}
 }
 
-
 if {$::windowingsystem eq "aqua"} {
     bind all <Mod1-Shift-Key-F> {+toggle_fullscreen %W}
 } else {
     bind all <F11> {+toggle_fullscreen %W}
 }
 
-# CordArrows
-# this adds arrowheads to the ends of cords when in editmode in
-# order to show the direction that the messages are flowing
 
-proc add_arrows_to_cords {mytoplevel} {
-    if { ! [winfo exists $mytoplevel] } {return}
+#
+# Change Background Color in Edit Mode
+#
+
+proc change_background_color_in_editmode {mytoplevel eventname} {
     if {$mytoplevel eq ".pdwindow"} {return}
     set tkcanvas [tkcanvas_name $mytoplevel]
+        if { ! [winfo exists $mytoplevel] } {return}
     if {$::editmode($mytoplevel) == 1} {
-        $tkcanvas itemconfigure cord -arrow last
+        $tkcanvas configure -background "lavender"
     } else {
-        $tkcanvas itemconfigure cord -arrow none
+        $tkcanvas configure -background white
     }
 }
 
-bind PatchWindow <<EditMode>> {+add_arrows_to_cords %W}
+bind PatchWindow <<EditMode>> {+change_background_color_in_editmode %W editmode}
+bind PatchWindow <<Loaded>> {+change_background_color_in_editmode %W loaded}
+
+
+
+
+# META TCL/TK prompt plugin
+# META DESCRIPTION enables a wee Tcl/Tk cmdline within the Pd-console
+# META AUTHOR IOhannes m zmˆlnig <zmoelnig@umlaeute.mur.at>, Hans-Christoph Steiner <hans@eds.org>
+# META VERSION 0.1
+
+package require pdwindow 0.1
+
+## first check if the Pd-runtime provides a tcl_entry (and use it)
+if {[catch ::pdwindow::create_tcl_entry errorname]} {
+
+## if that fails, we need to provide our own (code shamelessly taken from Pd-0.46)
+
+namespace eval ::tclprompt:: {
+    variable tclentry {}
+    variable tclentry_history {"console show"}
+    variable history_position 0
+}
+
+proc ::tclprompt::eval_tclentry {} {
+    variable tclentry
+    variable tclentry_history
+    variable history_position 0
+    if {$tclentry eq ""} {return} ;# no need to do anything if empty
+    if {[catch {uplevel #0 $tclentry} errorname]} {
+        global errorInfo
+        switch -regexp -- $errorname {
+            "missing close-brace" {
+                ::pdwindow::error [concat [_ "(Tcl) MISSING CLOSE-BRACE '\}': "] $errorInfo]\n
+            } "missing close-bracket" {
+                ::pdwindow::error [concat [_ "(Tcl) MISSING CLOSE-BRACKET '\]': "] $errorInfo]\n
+            } "^invalid command name" {
+                ::pdwindow::error [concat [_ "(Tcl) INVALID COMMAND NAME: "] $errorInfo]\n
+            } default {
+                ::pdwindow::error [concat [_ "(Tcl) UNHANDLED ERROR: "] $errorInfo]\n
+            }
+        }
+    }
+    lappend tclentry_history $tclentry
+    set tclentry {}
+}
+
+
+proc ::tclprompt::get_history {direction} {
+    variable tclentry_history
+    variable history_position
+
+    incr history_position $direction
+    if {$history_position < 0} {set history_position 0}
+    if {$history_position > [llength $tclentry_history]} {
+        set history_position [llength $tclentry_history]
+    }
+    .pdwindow.tclprompt.entry delete 0 end
+    .pdwindow.tclprompt.entry insert 0 \
+        [lindex $tclentry_history end-[expr $history_position - 1]]
+}
+
+
+proc ::tclprompt::validate_tcl {} {
+    variable tclentry
+    if {[info complete $tclentry]} {
+        .pdwindow.tclprompt.entry configure -background "white"
+    } else {
+        .pdwindow.tclprompt.entry configure -background "#FFF0F0"
+    }
+}
+
+#--create tcl entry-----------------------------------------------------------#
+
+proc ::tclprompt::create {} {
+    # Tcl entry box frame
+    frame .pdwindow.tclprompt -borderwidth 0
+    pack .pdwindow.tclprompt -side bottom -fill x -before .pdwindow.text
+    label .pdwindow.tclprompt.label -text [_ "Tcl:"] -anchor e
+    pack .pdwindow.tclprompt.label -side left
+    entry .pdwindow.tclprompt.entry -width 200 \
+       -exportselection 1 -insertwidth 2 -insertbackground blue \
+       -textvariable ::tclprompt::tclentry -font {$::font_family -12}
+    pack .pdwindow.tclprompt.entry -side left -fill x
+
+# bindings for the Tcl entry widget
+    bind .pdwindow.tclprompt.entry <$::modifier-Key-a> "%W selection range 0 end; break"
+    bind .pdwindow.tclprompt.entry <Return> "::tclprompt::eval_tclentry"
+    bind .pdwindow.tclprompt.entry <Up>     "::tclprompt::get_history 1"
+    bind .pdwindow.tclprompt.entry <Down>   "::tclprompt::get_history -1"
+    bind .pdwindow.tclprompt.entry <KeyRelease> +"::tclprompt::validate_tcl"
+
+    bind .pdwindow.text <Key-Tab> "focus .pdwindow.tclprompt.entry; break"
+
+#    pack .pdwindow.tclprompt
+}
+
+
+::tclprompt::create
+}
+
+pdtk_post "loaded tclprompt-plugin\n"
