6404abe6c2b39c96520d74a31d3657aa4fb43920
megrimm <megrimm@gmail.com>
2016-07-09 13:56:05 -0400

    add items

--- fresh-plugin/menutree.tcl
+++ fresh-plugin/menutree.tcl
@@ -1,19 +1,19 @@
 
  { "fresh audio"
  	{ "audio playback"
-        {  fresh/snd.player~ fresh/snd.phaseloop~ fresh/snd.slicebonk~} 
+        { fresh/snd.player~ fresh/snd.phaseloop~ fresh/snd.slicebonk~ } 
 	} 
     { "audio effects"
-        {  fresh/fx.bitcrush~ fresh/fx.chorus~ fresh/fx.delaydub~ fresh/fx.distortion~ fresh/fx.freeverb~ fresh/fx.fuzz~ fresh/fx.grainbash~ fresh/fx.karplus~ fresh/fx.omnifilter~ fresh/fx.streamstretch~ fresh/fx.stutter~ fresh/fx.vocoder~ fresh/fx.volattack~ fresh/fx.wobble~ } 
+        { fresh/fx.bitcrush~ fresh/fx.chorus~ fresh/fx.delaydub~ fresh/fx.distortion~ fresh/fx.freeverb~ fresh/fx.fuzz~ fresh/fx.grainbash~ fresh/fx.karplus~ fresh/fx.omnifilter~ fresh/fx.streamstretch~ fresh/fx.stutter~ fresh/fx.vocoder~ fresh/fx.volattack~ fresh/fx.wobble~ } 
 	} 
 	{ "audio mixing"
-        {  fresh/mix.between~ fresh/mix.dynadc~ fresh/mix.dyncatch~ fresh/mix.dynthrow~ fresh/mix.output~ fresh/mix.recorder~ } 
+        { fresh/mix.between~ fresh/mix.dynadc~ fresh/mix.dyncatch~ fresh/mix.dynthrow~ fresh/mix.output~ fresh/mix.recorder~ } 
 	} 
 	{ "audio equillization"
-        {  fresh/snd.eq.compressor~ fresh/snd.eq.punch~ fresh/snd.eq.riaa~ } 
+        { fresh/snd.eq.compressor~ fresh/snd.eq.punch~ fresh/snd.eq.riaa~ } 
 	} 
 	{ "audio synthesizers"
-        {  fresh/syn.acbass~ fresh/syn.bassdrum~ fresh/syn.fm8~ fresh/syn.hihat~ fresh/syn.rndwav~ fresh/syn.snare~ fresh/syn.speech~ } 
+        { fresh/syn.acbass~ fresh/syn.bassdrum~ fresh/syn.fm8~ fresh/syn.hihat~ fresh/syn.rndwav~ fresh/syn.snare~ fresh/syn.speech~ } 
 	}
 	{ "audio sound design"
         { fresh/sd.wind~ fresh/sd.nat.rumble~ fresh/sd.fire.hissing~ fresh/sd.fire.lapping~ fresh/sd.fire.forest~ fresh/sd.fire.creaking~ fresh/sd.fire.crackles~ } 
@@ -76,49 +76,49 @@
 
 { "vanilla" 
 	{ "glue"
-		{  bang float symbol int send receive select route pack unpack trigger spigot moses until print makefilename change swap value  } 
+		{ bang float symbol int send receive select route pack unpack trigger spigot moses until print makefilename change swap value } 
 	} 
 	{ "time"  
-		{  delay metro line timer cputime realtime pipe }  
+		{ delay metro line timer cputime realtime pipe }  
 	}
 	{ "tables"  
-		{ tabread tabread4 tabwrite soundfiler}  
+		{ tabread tabread4 tabwrite soundfiler }  
 	}
 	{ "subwindows"  
-		{ pd table inlet outlet inlet~ outlet~}  
+		{ inlet outlet inlet~ outlet~ table pd }  
 	}
 	{ "data templates"  
-		{ struct drawcurve filledcurve drawpolygon filledpolygon plot drawnumber}  
+		{ struct drawcurve filledcurve drawpolygon filledpolygon plot drawnumber }  
 	}
 	{ "accessing data"  
-		{ pointer get set element getsize setsize append sublist}  
+		{ pointer get set element getsize setsize append sublist }  
 	}
 	{ "input output"  
-		{ loadbang openpanel savepanel key keyup keyname}  
+		{ loadbang openpanel savepanel key keyup keyname }  
 	} 
 	{ "networks"  
-		{ netsend netreceive}  
+		{ netsend netreceive }  
 	} 
 	{ "storage"  
-		{ qlist textfile bag poly}  
+		{ qlist textfile bag poly }  
 	} 
 } 
 
 { "vanilla math" 
 	{ "arithmetic"
-		{  + - * / pow  } 
+		{ + - * / pow } 
 	} 
 	{ "relational tests"  
-		{  == != > < >= <= }  
+		{ == != > < >= <= }  
 	}
 	{ "bit twiddling"
-		{  & && ||| %  }  
+		{ & && ||| % }  
 	}  
 	{ "convert acoustical units"
-		{  mtof ftom powtodb rmstodb dbtopow dbtorms}  
+		{ mtof ftom powtodb rmstodb dbtopow dbtorms }  
 	}  
 	{ "higher math"
-		{ mod div sin cos tan atan atan2 sqrt log exp abs} 
+		{ mod div sin cos tan atan atan2 sqrt log exp abs } 
 	}
 	{ "lower math"
 		{ random expr } 
@@ -133,40 +133,40 @@
 
 { "vanilla midi" 
 	{ "midi input"
-		{  notein ctlin pgmin bendin touchin polytouchin midiin sysexin  } 
+		{ notein ctlin pgmin bendin touchin polytouchin midiin sysexin } 
 	} 
 	{ "convert acoustical units"  
-		{  mtof~ ftom~ rmstodb~ dbtorms~ rmstopow~ powtorms~ }  
+		{ mtof~ ftom~ rmstodb~ dbtorms~ rmstopow~ powtorms~ }  
 	}
 } 
 
 { "vanilla audio" 
 	{ "glue"
-		{  dac~ adc~ sig~ line~ vline~ threshold~ snapshot~ vsnapshot~ bang~ samplerate~ send~ receive~ throw~ cathc~ block~ switch~ readsf~ writesf~  } 
+		{ dac~ adc~ sig~ line~ vline~ threshold~ snapshot~ vsnapshot~ bang~ samplerate~ send~ receive~ throw~ cathc~ block~ switch~ readsf~ writesf~ } 
 	} 
 	{ "oscillators and tables"  
-		{  phasor~ cos~ osc~ tabwrite~ tabplay~ tabread~ tabread4~ tabosc4~ tabsend~ tabreceive~ }  
+		{ phasor~ cos~ osc~ tabwrite~ tabplay~ tabread~ tabread4~ tabosc4~ tabsend~ tabreceive~ }  
 	}
 	{ "filters"  
-		{  vcf~ noise~ env~ hip~ lop~ bp~ biquad~ samphold~ print~ rpole~ rzero~ rzero_rev~ cpole~ czero~ }  
+		{ vcf~ noise~ env~ hip~ lop~ bp~ biquad~ samphold~ print~ rpole~ rzero~ rzero_rev~ cpole~ czero~ }  
 	} 
 	{ "delay"  
-		{  delwrite~ delread~ vd~ }  
+		{ delwrite~ delread~ vd~ }  
 	} 
 } 
 
 { "vanilla audio math" 
 	{ "arithmetic"
-		{  +~ -~ *~ /~  } 
+		{ +~ -~ *~ /~ } 
 	} 
 	{ "fourier transforms"  
-		{  fft~ ifft~ rfft~ rifft~ framp~ }  
+		{ fft~ ifft~ rfft~ rifft~ framp~ }  
 	}
 	{ "maximum or minimum of 2 inputs"  
-		{  max~ min~ }  
+		{ max~ min~ }  
 	} 
 	{ "misc"  
-		{  q8_rsqrt~ q8_sqrt~ wrap~ }  
+		{ q8_rsqrt~ q8_sqrt~ wrap~ }  
 	} 
 } 
 
@@ -174,12 +174,24 @@
 
 { "gem video" 
 	{ "input output"
-		{  pix_film pix_image pix_video } 
+		{ pix_film pix_image pix_video } 
 	} 
 	{ "effects"  
-		{  pix_gain pix_motionblur pix_kaleidoscope pix_lumaoffset pix_posterize pix_puzzle pix_rtx }  
+		{ pix_gain pix_motionblur pix_kaleidoscope pix_lumaoffset pix_posterize pix_puzzle pix_rtx }  
 	}
 	{ "mixing"
-		{  pix_mix pix_diff pix_add pix_subtract pix_multiply } 
+		{ pix_mix pix_diff pix_add pix_subtract pix_multiply } 
+	}
+} 
+
+{ "gem geometric" 
+	{ "geometric 2D"
+		{ square rectangle circle triangle sphere polygon trapazoid torus disk curve cuboid ripple mesh_square mesh_line } 
+	}
+	{ "geometric 3D"
+		{ cube tube cylinder cone surface3d sphere3d curve3d pqtorusknots newWave } 
+	}
+	{ "particles"
+		{ part_source part_draw part_killold part_head part_velocity part_color part_gravity part_size part_orbitpoint part_targetsize part_targetcolor part_render part_info part_sink } 
 	}
 } 
